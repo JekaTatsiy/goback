4d524e2287332152bc64d3eb0f39c59d01047dc1
JekaTatsiy <jeka05.09.2002@yandex.ru>
2022-07-16 20:38:44 +0300

    add type convert from string

--- err/err_test.go
+++ err/err_test.go
@@ -1,1 +1,12 @@
 package err
+
+import (
+	"fmt"
+	"testing"
+)
+
+func BaseTestError(t *testing.T) {
+	e1 := NewErr(E_NONE, "")
+	e2 := FromMsgf("err: %s", "some bad happened")
+	fmt.Println(e1, e2)
+}
